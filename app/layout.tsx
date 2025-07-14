6110f2f4de9a8ecebe7c5b3b61f9c9023f6fec3d
billyfranklim1 <billyfranklim@gmail.com>
2025-07-14 16:24:41 -0300

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,17 @@ export default function RootLayout({
     >
       <head>
         <SandpackCSS />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-M8LBWHFC6T"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-M8LBWHFC6T');
+            `,
+          }}
+        />
       </head>
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
